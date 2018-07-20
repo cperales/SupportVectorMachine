cfa1f7d9cc4c551fa2a950c3d653d9f44f38afed
cperales <cperales@uloyola.es>
2018-07-20 19:32:38 +0200

    Test added

---
+++ test/test_svm.py
@@ -0,0 +1,28 @@
+from svm import LinearSVM, KernelSVM
+from util.generate_data import generate_data
+from util.metric import accuracy
+import unittest
+
+
+class TestLinearSVM(unittest.TestCase):
+
+    def test_linear(self):
+        clf = LinearSVM()
+        X, y = generate_data()
+        clf.fit(X=X, y=y, soft=True)
+        acc = accuracy(clf=clf, X=X, y=y)
+        margin = 0.04
+        average = 0.95
+        condition = average - margin <= acc <= average + margin
+        self.assertEqual(condition, True)
+
+    def test_kernel(self):
+        clf = KernelSVM()
+        X, y = generate_data()
+        clf.fit(X=X, y=y, kernel_type='rbf', k=1.0)
+        acc = accuracy(clf=clf, X=X, y=y)
+        margin = 0.04
+        average = 0.95
+        condition = average - margin <= acc <= average + margin
+        self.assertEqual(condition, True)
+
